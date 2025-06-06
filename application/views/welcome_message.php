4089510b5e7c1c14a500b663710a1c97c72474bd
Anggy Tatiana Peranquive Gomez <tatianaperanquive@gmail.com>
2025-06-06 11:09:07 -0500

    Estilizar formularion con boostrap

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -9,15 +9,53 @@
 
  <link href="CSS/estilos.css" rel="stylesheet">
 </head>
-<body >
-	<form action="guardar" method="post">
-		<input type="text" name='first_name' id="first_name" placeholder="first_name">
-		<input type="text" name='last_name' id="last_name" placeholder="first_name">
-		<input type="text" name='email' id="email" placeholder="first_name">
-		<input type="text" name='gender' id="gender" placeholder="first_name">
-		<input type="text" name='telephone' id="telephone" placeholder="first_name">
-		<input type="text" name='age' id="age" placeholder="first_name">
-		<button type="submit">Guardar</button>
-	</form>
+<body class="d-flex flex-column h-100">
+
+	    <main class="flex-shrink-0">
+        <div class="container">
+            <h3 class="my-3">New User</h3>
+ 
+            <form action="guardar" class="row g-3" method="post" autocomplete="off">
+                <div class="col-md-8">
+                    <label for="nombre" class="form-label">First Name</label>
+					<input type="text" class="form-control" id="nombre" name='first_name' id="first_name" placeholder="first_name" required>   
+                </div>
+              
+				<div class="col-md-8">
+                    <label for="nombre" class="form-label">Last Name</label>
+					<input type="text" class="form-control" id="nombre" name='last_name' id="last_name" placeholder="last_name" required>                  
+                </div>
+
+                <div class="col-md-6">
+                    <label for="email" class="form-label">Email</label>
+                    <input type="email" class="form-control" name='email' id="email" placeholder="email">
+                </div>
+
+                <div class="col-md-6">
+                    <label for="gender" class="form-label">gender</label>
+                    <select class="form-select" id="gender" name="gender" required>
+						<option value="">Please select one…</option>
+						<option value="female">Female</option>
+						<option value="male">Male</option>
+						<option value="non-binary">Non-Binary</option>
+						<option value="other">Other</option>
+						<option value="Prefer not to answer">Perfer not to Answer</option>
+					</select>
+                </div>
+
+				<div class="col-md-6">
+                    <label for="telephone" class="form-label">Telephone</label>
+                    <input type="telephone" class="form-control" name="telephone" id="telephone" placeholder="telephone" >
+                </div>
+
+                <div class="col-12">
+
+                    <button type="submit" class="btn btn-primary">Save</button>
+                </div>
+
+            </form>
+
+        </div>
+    </main>
 </body>
 </html>
